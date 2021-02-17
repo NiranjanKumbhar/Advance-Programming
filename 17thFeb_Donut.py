673a950535b00a8ebcc7d2ca09e2bb0778cba04b
NiranjanKumbhar <niranjankumbhar@hotmail.com>
2021-02-17 10:02:04 +0000

    Second change

--- 17thFeb_Donut.py
+++ 17thFeb_Donut.py
@@ -1,3 +1,17 @@
 def donuts(count):
+    """
+    This is test
+
+    >>> donuts(3))
+    '3'
+    >>> donuts(15)
+    'Many'
     
-    return
+    """
+    if(count>10):
+        return 'Number of Donuts : Many'
+    return 'Number of Donuts :' + str(count)
+
+
+print(donuts(3))
+print(donuts(15))
